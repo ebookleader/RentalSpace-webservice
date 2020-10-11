79973907ffbc11d7c1e48b76a9ca1b9fcbb33d30
JEunLe <hemslje@naver.com>
2020-10-11 23:52:25 +0900

    index 페에지 3/4 완성

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,32 +1,24 @@
 $(document).ready(function() {
 
-    // index.html
-    // 지역 선택 영역
-    $("#location").keyup(function() {
-        var k = $(this).val();
-        if(k=="서울"){
-          $("#location-link").text("서울");
-        }
+  // index.html
+  // 지역 선택 영역
+  $("#seoul,#incheon,#daegu,#daejeon,#busan,#gwangju,#ulsan,#saejong,#gyeonggi,#gangwon,#chungn,#chungs,#junn,#juns,#gyeongn,#gyeongs,#jeju").click(function() {
+    var k = $(this).text();
+    $("#location-dropdown").text(k);
+  })
 
-    });
+  // 카테고리 선택 영역
+  $("#category-instrument,#category-sing,#category-dance,#category-cooking,#category-musical,#category-study").click(function() {
+    var k = $(this).text();
+    $("#category-dropdown").text(k);
+  })
 
-    $("#location-link").click(function() {
-      var k = $("#location").val();
-      $("#location-dropdown").text(k);
-    });
-
-    // 카테고리 선택 영역
-    $("#category-instrument,#category-sing,#category-dance,#category-cooking,#category-musical,#category-study").click(function() {
-       var k = $(this).text();
-       $("#category-dropdown").text(k);
-    })
-
-    // Search button
-    $("#mainpage-search-button").click(function() {
-      var location = $("#location").val();
-      var category = $("#category-dropdown").text();
-      alert(location);
-      alert(category);
-    })
+  // Search button
+  $("#mainpage-search-button").click(function() {
+    var location = $("#location-dropdown").text();
+    var category = $("#category-dropdown").text();
+    alert(location);
+    alert(category);
+  })
 
 })
