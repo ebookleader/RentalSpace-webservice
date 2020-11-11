75358133751bee97e8e6a751723a37be2713f44a
JEunLe <hemslje@naver.com>
2020-11-11 18:56:19 +0900

    facility, notice, policy javascript&html modified

--- build/resources/main/static/js/main.js
+++ build/resources/main/static/js/main.js
@@ -22,29 +22,128 @@ $(document).ready(function() {
   })
 
     // 동적 input(facility) 추가
-    var maxField = 10;
-    var facility_wrapper = $('#facility_append');
-    var cnt = 1;
 
-    $('.facility_add_btn').click(function() {
-      if(cnt<maxField) {
-        cnt++;
-        var fieldHtml = '<div class="form-inline"><div class="input-group-prepend"><span class="input-group-text" id="facility-'+cnt+'">'+cnt+'</span></div><input type="text" class="form-control" name="facility" id="p_facility'+cnt+'"><a href="#aa" class="facility_remove_btn"><i class="fa fa-minus ml-3"></i></a></div>'
-        $('#facility_append').append(fieldHtml);
-      }
-    });
+      var maxField = 10;
+      var facility_wrapper = $('.facility_append');
+      var cnt = 1;
+      var facilityList = new Array(10);
 
-    $(facility_wrapper).on('click','.facility_remove_btn', function(e) {
-      e.preventDefault();
-      cnt--;
-      $('#facility_append').empty();
-      for(var i=2;i<=cnt;i++) {
-        var fieldHtml = '<div class="form-inline"><div class="input-group-prepend"><span class="input-group-text" id="facility-'+i+'">'+i+'</span></div><input type="text" class="form-control" name="facility" id="p_facility'+i+'" aria-label="Username" aria-describedby="facility-'+i+'"><a href="#aa" class="facility_remove_btn"><i class="fa fa-minus ml-3"></i></a></div>'
-        $('#facility_append').append(fieldHtml);
-      }
-    });
+      // 추가
+      $('.facility_add_btn').click(function() {
+        if(cnt<maxField) {
+          cnt++;
+          var fieldHtml = '<div class="form-inline"><div class="input-group-prepend"><span class="input-group-text" id="facility-'+cnt+'">'+cnt+'</span></div><input type="text" class="form-control" name="facility" id="p_facility'+cnt+'"></div>'
+          var fieldHtml_with_btn = '<div class="form-inline"><div class="input-group-prepend"><span class="input-group-text" id="facility-'+cnt+'">'+cnt+'</span></div><input type="text" class="form-control" name="facility" id="p_facility'+cnt+'"><div id="remove_btn_div'+cnt+'"><a href="#aa" class="facility_remove_btn" id="a_facility_remove_btn"><i class="fa fa-minus ml-3"></i></a></div></div>'
+          if(cnt>2) {
+            var parent = document.getElementById("remove_btn_div"+(cnt-1));
+            parent.removeChild(document.getElementById("a_facility_remove_btn"));
+          }
+          $('.facility_append').append(fieldHtml_with_btn);
+        }
+      });
 
-    // 동적 input notice 추가
+      // 삭제
+      $(facility_wrapper).on('click','.facility_remove_btn', function(e) {
+        e.preventDefault();
+        for(var i=0;i<cnt;i++) {
+          facilityList[i] = document.getElementById("p_facility"+(i+1)).value;
+        }
+        cnt--;
+        $('.facility_append').empty();
+        for(var i=2;i<=cnt;i++) {
+          var fieldHtml = '<div class="form-inline"><div class="input-group-prepend"><span class="input-group-text" id="facility-'+i+'">'+i+'</span></div><input type="text" class="form-control" name="facility" id="p_facility'+i+'"></div>'
+          var fieldHtml_with_btn = '<div class="form-inline"><div class="input-group-prepend"><span class="input-group-text" id="facility-'+i+'">'+i+'</span></div><input type="text" class="form-control" name="facility" id="p_facility'+i+'"><div id="remove_btn_div'+i+'"><a href="#aa" class="facility_remove_btn" id="a_facility_remove_btn"><i class="fa fa-minus ml-3"></i></a></div></div>'
+          if(i==cnt) {
+            $('.facility_append').append(fieldHtml_with_btn);
+          }
+          else {
+            $('.facility_append').append(fieldHtml);
+          }
+          document.getElementById("p_facility"+i).value = facilityList[i-1];
+        }
+      });
+
+      // 동적 input(notice) 추가
+      var notice_wrapper = $('.notice_append');
+      var ncnt = 1;
+      var noticeList = new Array(10);
+
+      // 추가
+      $('.notice_add_btn').click(function() {
+        if(ncnt<maxField) {
+          ncnt++;
+          var fieldHtml = '<div class="form-inline"><div class="input-group-prepend"><span class="input-group-text" id="notice-'+ncnt+'">'+ncnt+'</span></div><input type="text" class="form-control" name="notice" id="p_notice'+ncnt+'"></div>'
+          var fieldHtml_with_btn = '<div class="form-inline"><div class="input-group-prepend"><span class="input-group-text" id="notice-'+ncnt+'">'+ncnt+'</span></div><input type="text" class="form-control" name="notice" id="p_notice'+ncnt+'"><div id="notice_remove_btn_div'+ncnt+'"><a href="#aa" class="notice_remove_btn" id="a_notice_remove_btn"><i class="fa fa-minus ml-3"></i></a></div></div>'
+          if(ncnt>2) {
+            var parent = document.getElementById("notice_remove_btn_div"+(ncnt-1));
+            parent.removeChild(document.getElementById("a_notice_remove_btn"));
+          }
+          $('.notice_append').append(fieldHtml_with_btn);
+        }
+      });
+
+      // 삭제
+      $(notice_wrapper).on('click','.notice_remove_btn', function(e) {
+        e.preventDefault();
+        for(var i=0;i<ncnt;i++) {
+          noticeList[i] = document.getElementById("p_notice"+(i+1)).value;
+        }
+        ncnt--;
+        $('.notice_append').empty();
+        for(var i=2;i<=ncnt;i++) {
+          var fieldHtml = '<div class="form-inline"><div class="input-group-prepend"><span class="input-group-text" id="notice-'+i+'">'+i+'</span></div><input type="text" class="form-control" name="notice" id="p_notice'+i+'"></div>'
+          var fieldHtml_with_btn = '<div class="form-inline"><div class="input-group-prepend"><span class="input-group-text" id="notice-'+i+'">'+i+'</span></div><input type="text" class="form-control" name="notice" id="p_notice'+i+'"><div id="notice_remove_btn_div'+i+'"><a href="#aa" class="notice_remove_btn" id="a_notice_remove_btn"><i class="fa fa-minus ml-3"></i></a></div></div>'
+
+          if(i==ncnt) {
+            $('.notice_append').append(fieldHtml_with_btn);
+          }
+          else {
+            $('.notice_append').append(fieldHtml);
+          }
+          document.getElementById("p_notice"+i).value = noticeList[i-1];
+        }
+      });
+
+      // 동적 input(policy) 추가
+      var policy_wrapper = $('.policy_append');
+      var pcnt = 1;
+      var policyList = new Array(10);
+
+      // 추가
+      $('.policy_add_btn').click(function() {
+        if(pcnt<maxField) {
+          pcnt++;
+          var fieldHtml = '<div class="form-inline"><div class="input-group-prepend"><span class="input-group-text" id="policy-'+pcnt+'">'+pcnt+'</span></div><input type="text" class="form-control" name="policy" id="p_policy'+pcnt+'"></div>'
+          var fieldHtml_with_btn = '<div class="form-inline"><div class="input-group-prepend"><span class="input-group-text" id="policy-'+pcnt+'">'+pcnt+'</span></div><input type="text" class="form-control" name="policy" id="p_policy'+pcnt+'"><div id="policy_remove_btn_div'+pcnt+'"><a href="#aa" class="policy_remove_btn" id="a_policy_remove_btn"><i class="fa fa-minus ml-3"></i></a></div></div>'
+          if(pcnt>2) {
+            var parent = document.getElementById("policy_remove_btn_div"+(pcnt-1));
+            parent.removeChild(document.getElementById("a_policy_remove_btn"));
+          }
+          $('.policy_append').append(fieldHtml_with_btn);
+        }
+      });
+
+      // 삭제
+      $(policy_wrapper).on('click','.policy_remove_btn', function(e) {
+        e.preventDefault();
+        for(var i=0;i<pcnt;i++) {
+          policyList[i] = document.getElementById("p_policy"+(i+1)).value;
+        }
+        pcnt--;
+        $('.policy_append').empty();
+        for(var i=2;i<=pcnt;i++) {
+          var fieldHtml = '<div class="form-inline"><div class="input-group-prepend"><span class="input-group-text" id="policy-'+i+'">'+i+'</span></div><input type="text" class="form-control" name="policy" id="p_policy'+i+'"></div>'
+          var fieldHtml_with_btn = '<div class="form-inline"><div class="input-group-prepend"><span class="input-group-text" id="policy-'+i+'">'+i+'</span></div><input type="text" class="form-control" name="policy" id="p_policy'+i+'"><div id="policy_remove_btn_div'+i+'"><a href="#aa" class="policy_remove_btn" id="a_policy_remove_btn"><i class="fa fa-minus ml-3"></i></a></div></div>'
+
+          if(i==pcnt) {
+            $('.policy_append').append(fieldHtml_with_btn);
+          }
+          else {
+            $('.policy_append').append(fieldHtml);
+          }
+          document.getElementById("p_policy"+i).value = policyList[i-1];
+        }
+      });
 
 
 })
